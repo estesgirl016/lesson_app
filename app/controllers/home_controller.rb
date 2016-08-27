c3eaff768db49b722e4611fc518628ae29faa295
Justin <woodmansee1207@gmail.com>
2016-08-27 13:41:42 -0600

    added assigment buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   	if signed_in? && current_user.teacher?
   		redirect_to student_list_path(current_user.id)
   	elsif signed_in? && current_user.student?
-  		redirect_to lesson.where(user_id: current_user.user_id).order('lesson_date').last
+  		redirect_to assignment_index_path(Lesson.where(user_id: current_user.id).order('lesson_date').last)
   	else
   		render :index
   	end
