3c30a13622ae2b002cb5cc804df0592faec48ff2
Lonnie Horlacher <horlacher.lonnie@gmail.com>
2016-08-27 15:48:38 -0600

    home controller fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ApplicationController
   def index
   	if signed_in? && current_user.teacher?
   		redirect_to student_list_path(current_user.id)
-  	elsif signed_in? && current_user.student? && Lesson.where(user_id: current_user.id)
+  	elsif signed_in? && current_user.student? && Lesson.where(user_id: current_user.id).any?
+      binding.pry
   		redirect_to assignment_index_path(Lesson.where(user_id: current_user.id).order('lesson_date').last)
     elsif signed_in? && current_user.student?
-      redirect_to show_student(@current_user)
+      redirect_to show_student_path(current_user)
   	else
   		render :index
   	end
