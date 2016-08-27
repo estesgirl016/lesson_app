735005ba42f6a32ec507ce5335ea365c580165f5
BradytWind <bradywinward@gmail.com>
2016-08-27 11:18:54 -0600

    init commit Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,27 @@
 class LessonsController < ApplicationController
+  before_action :teacher, except: [:index, :new, :create]
+
+
   def index
+    @user_id = User_id.all.lesson_date
   end
 
   def new
+    @lesson = Lesson.new
   end
 
   def edit
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+    @lesson.destroy
+    redirect_to lesson_path
+  end 
+  
 end
